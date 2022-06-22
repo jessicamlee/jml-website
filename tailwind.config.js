b3342835ea04f227f28c5ae2e4283d0ff9b3dc13
jessicamlee <jessicamlee25@gmail.com>
2022-06-22 16:44:57 -0700

    Adds Tailwind and NPM
    Adds Tailwind and NPM

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
+module.exports = {
+  content: ["./**/*.{html,php}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        'vio': '0px 3px 5px rgba(43,4,98,0.65)',
+      }
+    },
+    colors: {
+      'winter': '#d3f7ff',
+      'violet': '#2b0462',
+      'ghost': '#f7fcfe',
+    }
+  },
+  plugins: [],
+}
