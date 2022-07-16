989f5d1bf0eb78a1b54491b5931e25eabd1664fc
jessicamlee <jessicamlee25@gmail.com>
2022-07-15 19:52:25 -0700

    Fixed syntax error in header styles
    Fixed syntax error in header styles

--- partials/global/header_projects.php
+++ partials/global/header_projects.php
@@ -10,9 +10,9 @@
         <div class="basis-1/2"></div>
         <nav class="basis-1/4 flex items-center justify-end font-semibold">
             <ul class="flex items-center">
-                <li class="px-4 text-md <?php if($active_page == "about") { echo "active"; } ?>"><a href="../about.php" title="Link to About">About</a></li>
-                <li class="px-4 text-md <?php if($active_page == "development") { echo "active"; } ?>"><a href="../projects.php" title="Link to Development Projects">Development</a></li>
-                <li class="pl-4 text-md <?php if($active_page == "contact") { echo "active"; } ?>"><a href="#contact" title="Link to Contact">Contact</a></li>
+                <li class="px-4 text-base <?php if($active_page == "about") { echo "active"; } ?>"><a href="../about.php" title="Link to About">About</a></li>
+                <li class="px-4 text-base <?php if($active_page == "development") { echo "active"; } ?>"><a href="../projects.php" title="Link to Development Projects">Development</a></li>
+                <li class="pl-4 text-base <?php if($active_page == "contact") { echo "active"; } ?>"><a href="#contact" title="Link to Contact">Contact</a></li>
             </ul>
         </nav>
     </div>
