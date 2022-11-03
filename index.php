48ffbb42705a475a5dc03f56d3b09fee20cb152f
jessicamlee <jessicamlee25@gmail.com>
2022-11-02 17:49:53 -0700

    Removed header-up and cleaned up darkmode
    Removed header-up and cleaned up darkmode

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
                     <div class="container intro-box">
                         <h1 data-aos="fade-right">Jessica Lee</h1>
                         <p class="career-role" data-aos="fade-right" data-aos-delay="300">Front&dash;End Development</p>
-                        <p class="career-goal flex" data-aos="fade-right" data-aos-delay="500">building interfaces worth experiencing&period;</p>
+                        <p class="career-goal flex" data-aos="fade-right" data-aos-delay="500">building interfaces for everyone to experience&period;</p>
                     </div>
                     <div class="blobwindow" data-aos="fade-up-left" data-aos-delay="500">
                         <div class="alien"></div>
