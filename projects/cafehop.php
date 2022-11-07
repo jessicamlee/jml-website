23a6f3754b19d958b239a1be91ff04a158049461
jessicamlee <jessicamlee25@gmail.com>
2022-11-06 19:24:51 -0800

    Fixed linking errors (reset, main.js)
    Fixed linking errors (reset, main.js)

--- projects/cafehop.php
+++ projects/cafehop.php
@@ -31,9 +31,6 @@
             </main>
             <?php include('../partials/global/footer.php'); ?>
         </div>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-        <script src="../scripts/scroll-lock.js"></script>
-        <script src="../scripts/main.js"></script>
+        <?php include('../partials/projects/footer_links.php'); ?>
     </body>
 </html>
