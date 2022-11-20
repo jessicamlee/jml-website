b4dd7fd0ae74f1b78c8dfaccc86ac07fbd46f37a
jessicamlee <jessicamlee25@gmail.com>
2022-11-20 13:25:00 -0800

    Add Gulp Workflow for SCSS & JS files
    Add Gulp Workflow for SCSS & JS files

--- index.php
+++ index.php
@@ -18,8 +18,8 @@
             </main>
             <?php include('partials/global/footer.php'); ?>
         </div>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-        <script src="scripts/main.js"></script>
+        <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script> -->
+        <!-- <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script> -->
+        <script src="build/js/main.js"></script>
     </body>
 </html>
