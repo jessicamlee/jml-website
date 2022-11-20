bf9d254a055bf4831f7fa3ff7c4f755fc73321e5
jessicamlee <jessicamlee25@gmail.com>
2022-11-20 13:03:48 -0800

    Add overview info for CMJSH Agency card
    Add overview info for CMJSH Agency card

--- partials/home/latest-projects.php
+++ partials/home/latest-projects.php
@@ -4,9 +4,9 @@
         <ul class="projects-collection flex">
             <?php
                 $project_title = 'Spring Local 2022';
-                $project_type = 'Responsive Homepage';
+                $project_type = 'Responsive e&dash;commerce Website';
                 $project_pg_link = 'locloclothing';
-                $project_img_src = 'loclo_cover.jpg';
+                $project_img_src = 'loclo_cover';
                 $project_img_alt = 'LOCLO Homepage Project Cover Photo';
                 $live_link = 'https://locloclothing.jessicamlee.dev/';
                 $code_link = 'https://github.com/jessicamlee/loclo-clothing';
@@ -16,7 +16,7 @@
                 $project_title = 'Caf&eacute; Hop App';
                 $project_type = 'Android&dash;based Mobile Application';
                 $project_pg_link = 'cafehop';
-                $project_img_src = 'cafehop_cover.jpg';
+                $project_img_src = 'cafehop_cover';
                 $project_img_alt = 'Cafe Hop App Cover Photo';
                 $live_link = 'https://cafehop.jessicamlee.dev/';
                 $code_link = 'https://github.com/jessicamlee/cafehop';
@@ -26,10 +26,20 @@
                 $project_title = 'My To&dash;Do List';
                 $project_type = 'Temporary To&dash;Do List';
                 $project_pg_link = 'mytodo';
-                $project_img_src = 'todo_cover.jpg';
+                $project_img_src = 'todo_cover';
                 $project_img_alt = 'My To-Do List Cover Photo';
                 $live_link = 'https://mytodolist.jessicamlee.dev/';
                 $code_link = 'https://github.com/jessicamlee/todolist';
+                include('partials/home/project-card.php');
+            ?>
+            <?php
+                $project_title = 'CMJSH Agency';
+                $project_type = 'Responsive Agency Landing Page';
+                $project_pg_link = 'cmjshagency';
+                $project_img_src = 'default_cover';
+                $project_img_alt = 'CMJSH Agency Website Cover Photo';
+                $live_link = 'https://github.com/jessicamlee/CMJSH-agency';
+                $code_link = 'https://github.com/jessicamlee/CMJSH-agency';
                 include('partials/home/project-card.php');
             ?>
         </ul>
