9193ba70a41142c28733d0b5eee7ba9a645ed1b3
jessicamlee <jessicamlee25@gmail.com>
2023-08-22 23:31:37 -0700

    Create web-development page

---
+++ partials/development/development-intro.php
@@ -0,0 +1,6 @@
+<section class="subpage-intro subpg-section">
+    <div class="container">
+        <h1>Web Development Projects</h1>
+        <p class="pg-preface">An array of Jessica&apos;s personal web development projects&comma; focusing on front&dash;end development with exploration of full&dash;stack development&period;</p>
+    </div>
+</section>
---
+++ web-development.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en-CA">
+    <?php 
+        $title_text = "Web Development &vert; Jessica Lee Front&dash;End Developer &amp; Web Designer";
+        include('partials/global/head.php'); 
+    ?>
+    <body>
+        <div class="wrapper">
+        <?php include('partials/global/header.php'); ?>
+            <main>
+                <?php include('partials/development/development-intro.php'); ?>
+                <?php include('partials/development/development.php'); ?>
+                <?php include('partials/global/contact.php'); ?>
+            </main>
+            <?php include('partials/global/footer.php'); ?>
+        </div>
+        <script src="build/js/main.js"></script>
+    </body>
+</html>
