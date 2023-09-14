c08bd043071a06ba72d860f4823bbac623c7c5a0
jessicamlee <jessicamlee25@gmail.com>
2023-09-13 23:15:27 -0700

    Add and rename UX cards

---
+++ partials/cards/ux-work-card.php
@@ -0,0 +1,26 @@
+<?php
+
+    $ux_project_title = $ux_project_title ?? '';
+    $ux_project_img_src = $ux_project_img_src ?? '';
+    $ux_project_img_alt = $ux_project_img_alt ?? '';
+    $ux_project_type = $ux_project_type ?? '';
+    $ux_project_roles = $ux_project_roles ?? '';
+    $ux_project_duration = $ux_project_duration ?? '';
+    $prototype_link = $prototype_link ?? '';
+    $case_study_link = $case_study_link ?? '';
+
+?><div class="container flex cs-summary cs-flex-tablet">
+    <div>
+        <h1><?php echo $ux_project_title; ?></h1>
+        <p><span class="bolded">Project:</span> <?php echo $ux_project_type; ?></p>
+        <p><span class="bolded">Role:</span> <?php echo $ux_project_roles; ?></p>
+        <p><span class="bolded">Duration:</span> <?php echo $ux_project_duration; ?></p>
+        <div class="btn-container flex cs-btns">
+            <a class="btn-primary" tabindex="0" href="<?php echo $case_study_link; ?>" target="_blank" title="View Slide Deck">View Slide Deck</a>
+            <a class="btn-secondary" tabindex="0" href="<?php echo $prototype_link; ?>" target="_blank" title="Visit Live Site">Visit Live Site</a>
+        </div>
+    </div>
+    <div class="img-container cs-img-container">
+        <img class="img-thumb" src="../images/projects/<?php echo $ux_project_img_src; ?>.jpg" alt="<?php echo $ux_project_img_alt; ?>">
+    </div>
+</div>
