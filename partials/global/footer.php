79fa96a9cc3dbd78db5556346e8fbd152091f2c6
jessicamlee <jessicamlee25@gmail.com>
2023-11-22 20:05:29 -0800

    Update Nav Menu and Home layout

--- partials/global/footer.php
+++ partials/global/footer.php
@@ -1,6 +1,6 @@
 <footer>
     <div class="container">
-        <p>Built with 💙 &vert; Fueled by matcha 🍵</p>
+        <p>Fueled by matcha 🍵</p>
         <p>&copy; 2023 Jessica Lee&period; All Rights Reserved&period;</p>
     </div>
 </footer>
