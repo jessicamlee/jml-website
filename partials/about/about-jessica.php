502f2c62644cfe0339a248fb63ca21a69b694806
jessicamlee <jessicamlee25@gmail.com>
2023-11-28 14:05:33 -0800

    Update about description

--- partials/about/about-jessica.php
+++ partials/about/about-jessica.php
@@ -2,7 +2,7 @@
     <div class="container bio flex">
         <div class="bio-desc">
             <h1 data-aos="fade-right">Hi&comma; Jessica here&excl;</h1>
-            <p data-aos="fade-left">I&apos;m a 🌱 UX Designer with a product thinking and curious learning mindset and a background in English Literature&comma; Creative Writing&comma; Content Strategy&comma; and Front&dash;end Development&period;</p>
+            <p data-aos="fade-left">I&apos;m a 🌱 Product&sol;UX Designer with a product thinking and curious learning mindset and a background in English Literature&comma; Creative Writing&comma; Content Strategy&comma; and Front&dash;end Development&period;</p>
             <p data-aos="fade-left" data-aos-delay="350">📚 My hobby of analyzing stories led me to past experiences as a content writer and managing editor that further refined my storytelling skills&period;</p>
             <p data-aos="fade-left" data-aos-delay="400">🌿 I now strive to bridge the gap between businesses and their end&dash;users by creating digital solutions to solve problems and make a positive impact on affected communities and the environment&period;</p>
             <p data-aos="fade-left" data-aos-delay="450">🍵 Besides unwinding with a cup of matcha&comma; I spend time building new projects&comma; reading manga&comma; dissecting films and anime&comma; trying different yoga videos&comma; and getting my steps exploring a non&dash;digital city&period;</p>
