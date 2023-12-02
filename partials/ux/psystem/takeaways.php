d005a7e6b098a9fc6d379cb362cf8afd9371b5fe
jessicamlee <jessicamlee25@gmail.com>
2023-12-01 21:45:37 -0800

    Update hover styles

--- partials/ux/psystem/takeaways.php
+++ partials/ux/psystem/takeaways.php
@@ -4,12 +4,12 @@
         <p>Thrilled to lead the redesign&comma;  I prepared the team for Psystem&apos;s upcoming campaigns with the informative ad landing pages&period; <span class="bolded">The gratitude levels were off the charts&excl;</span></p>
         <div class="img-container cs-body-img-container">
             <img class="img-thumb" src="images/case-studies/psystem/psystem_recommendation.jpg" alt="Three highlighted aspects of Jessica's impact from a LinkedIn Recommendation note">
-            <p class="cs-img-caption">Highlighting my impact on Psystem&apos;s growth from Psystem&apos;s founder&apos;s LinkedIn recommendation note&period;</p>
+            <p class="cs-img-caption">My impact on Psystem&apos;s growth from Psystem&apos;s founder&apos;s LinkedIn recommendation note&period;</p>
         </div>
         <p>I also implemented a Cookie Consent tool that tracked <span class="bolded">more than 200 monthly view visits</span>&comma; which <span class="bolded">increased by around 33&#x0025; in website visits</span>&period;</p>
         <div class="img-container cs-body-img-container">
             <img class="img-thumb" src="images/case-studies/psystem/psystem_myimpact.jpg" alt="Screencap of Elfsight's Cookie Consent notice that website visits have surpassed the 200 view limit for the month of May 2023">
-            <p class="cs-img-caption">Highlighting the positive impact of more website visits tracked by Elfsight&apos;s Cookie Consent tool&period;</p>
+            <p class="cs-img-caption">The positive impact of more website visits tracked by Elfsight&apos;s Cookie Consent tool&period;</p>
         </div>
         <h2>Wisdom Gained&comma; Onwards I go</h2>
         <h3>🌱 Lessons Learned&colon; Design&comma; Manage&comma; Learn&comma; Repeat&excl;</h3>
@@ -26,6 +26,6 @@
             <li><span class="bolded">Unroll the usability red carpet with tests</span> wbeyond internal testers to catch new pain points and opportunities for a chronic pain&dash;friendly website&period;</li>
             <li><span class="bolded">Embark on a quest to map the website&dash;to&dash;mobile app journey</span> to create a smooth ride that aligns with Psystem&apos;s goals on both platforms&period;</li>
         </ul>
-        <p>🌿 <span class="bolded">Thank you for checking out my adventure&excl;</span> Got thoughts or questions&quest; Let&apos;s chat on <a href="https://www.linkedin.com/in/jessicamlee-/" target="_blank" title="Connect with Jessica on LinkedIn">LinkedIn&excl; I&apos;m excited to learn from your wisdom&excl; 🥳</p>
+        <p>🌿 <span class="bolded">Thank you for checking out my adventure&excl;</span> Got thoughts or questions&quest; Let&apos;s chat on <a href="https://www.linkedin.com/in/jessicamlee-/" target="_blank" title="Connect with Jessica on LinkedIn">LinkedIn</a>&excl; I&apos;m excited to learn from your wisdom&excl; 🥳</p>
     </div>
 </section>
