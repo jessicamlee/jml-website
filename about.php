79f56ed3054cb6a1c96b83d14a3bcaba82f15c64
jessicamlee <jessicamlee25@gmail.com>
2024-01-05 21:14:15 -0800

    Update about copy

--- about.php
+++ about.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en-CA">
     <?php 
-        $title_text = "About &vert; Jessica Lee&comma; Product&sol;UX Design&comma; Development&comma; Content Strategy";
+        $title_text = "About &vert; Jessica Lee&comma; Product Design&comma; UX&sol;UI Development&comma; Content Strategy";
         include('partials/global/head.php'); 
     ?>
     <body>
