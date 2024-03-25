549e4e7c2c33fa260cdff14d42564d8b1cd597e9
jessicamlee <jessicamlee25@gmail.com>
2024-03-24 22:00:21 -0700

    Update styles (typography and padding)

--- src/js/dark-mode-toggle.js
+++ src/js/dark-mode-toggle.js
@@ -4,4 +4,6 @@
 const checkbox = document.getElementById('checkbox');
 checkbox.addEventListener('change', ()=>{
     document.body.classList.toggle('light');
+
+    localStorage.setItem('checkbox', body.classList.contains('checkbox'));
 })
